3c66750843392d87fb3c812fb7213a029efee4c5
cmelgreen <cmelgreen@gmail.com>
2020-09-15 15:07:26 -0500

    update content.css

---
+++ build/precache-manifest.099a9465afb739713c6704c0267348ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7055f493ab13db40220f4d6bef8d3e6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "792e99a68c178fca6b8b",
+    "url": "/static/css/main.d8ec8b42.chunk.css"
+  },
+  {
+    "revision": "5e0d7f7fee62e3965732",
+    "url": "/static/js/2.dbad9285.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dbad9285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "792e99a68c178fca6b8b",
+    "url": "/static/js/main.dcdcacb2.chunk.js"
+  },
+  {
+    "revision": "3f3ac9fe654cae80f741",
+    "url": "/static/js/runtime-main.8435eee7.js"
+  },
+  {
+    "revision": "0b742926ea1fe5db50d86ab07b37612c",
+    "url": "/static/media/icon.0b742926.jpg"
+  }
+]);
--- src/linkbar/linkbar.js
+++ src/linkbar/linkbar.js
@@ -1,13 +1,10 @@
 import React from "react";
-import { InlineIcon } from "@iconify/react";
-import githubIcon from "@iconify/icons-logos/github-icon";
 
 import "./linkbar.css";
 
 export default function Linkbar() {
   return (
     <div class="link-bar">
-      <InlineIcon icon={githubIcon} />
     </div>
   );
 }
