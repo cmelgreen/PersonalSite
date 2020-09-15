bc71d9c91c5fd67b5441316214d80914b5d79e2b
cmelgreen <cmelgreen@gmail.com>
2020-09-15 14:48:50 -0500

    clear cache

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./App.css";
 
-import Linkbar from "./src/linkbar";
-import Title from "./src/title";
-import Content from "./src/content";
+import Linkbar from "./linkbar";
+import Title from "./title";
+import Content from "./content";
 
 export default function App() {
   return (
