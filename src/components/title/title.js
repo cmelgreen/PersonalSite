5dbb7ecc9300773ffd8c8a2472df7c3adb2a34fb
Cooper Melgreen <cmelgreen@gmail.com>
2020-10-13 12:53:54 -0500

    Update title.js

--- src/components/title/title.js
+++ src/components/title/title.js
@@ -7,7 +7,7 @@ export default function Title() {
       <picture>
         <img class="icon" src={require("./icon.jpg")} alt="icon" />
       </picture>
-      <div class="name">Cooper Melgreen - Webhook Test</div>
+      <div class="name">Cooper Melgreen - Webhook Test 2</div>
     </div>
   );
 }
