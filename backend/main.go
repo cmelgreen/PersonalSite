ddb74e2239750eee6e89078c3efa01e5751eb783
cmelgreen <cmelgreen@gmail.com>
2020-10-30 01:35:41 -0500

    update port to 8080

--- backend/main.go
+++ backend/main.go
@@ -14,9 +14,10 @@ func main() {
 	router.ServeFiles("/static/*filepath", http.Dir("/frontend/build/static"))
 	log.Println("Running...")
 
-	log.Fatal(http.ListenAndServe("0.0.0.0:80", router))
+	log.Fatal(http.ListenAndServe(":80", router))
 }
 
 func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Write([]byte("Hello"))
 	http.ServeFile(w, r, "/frontend/build/index.html")
 }
