148220c6015cd5b5f98d296196f3ed1e43bfb0e3
cmelgreen <cmelgreen@gmail.com>
2020-10-31 12:25:42 -0500

    move appspec scripts to scripts folder

--- frontend/src/components/title/title.js
+++ frontend/src/components/title/title.js
@@ -4,9 +4,6 @@ import "./title.css";
 export default function Title() {
   return (
     <div class="title">
-      {/* <picture>
-        {/* <img class="icon" src={require("./icon.jpg")} alt="icon" /> */}
-      </picture> */}
       <div class="name">Cooper Melgreen</div>
     </div>
   );
