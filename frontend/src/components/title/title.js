3b5b832d009b4ce2cf8b4073c3919eecadee6c2a
cmelgreen <cmelgreen@gmail.com>
2020-10-30 19:28:08 -0500

    remove pic

--- frontend/src/components/title/title.js
+++ frontend/src/components/title/title.js
@@ -4,9 +4,9 @@ import "./title.css";
 export default function Title() {
   return (
     <div class="title">
-      <picture>
+      {/* <picture>
         {/* <img class="icon" src={require("./icon.jpg")} alt="icon" /> */}
-      </picture>
+      </picture> */}
       <div class="name">Cooper Melgreen</div>
     </div>
   );
