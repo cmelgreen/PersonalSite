1726b7bdab93ac25d09fcce75e88dac40de0e486
cmelgreen <cmelgreen@gmail.com>
2020-11-02 16:32:42 -0600

    refactor

--- frontend/src/components/content/content.js
+++ frontend/src/components/content/content.js
@@ -4,7 +4,7 @@ import "./content.css"
 // const l = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 export default function Contenct() {
-  const items = GetContent()
+  const items = Content()
   return (
     <div className="content">
       {items}
@@ -12,8 +12,19 @@ export default function Contenct() {
   )
 }
 
+function Content() {
+  data = GetContent()
+
+  var c = []
+  console.log(data)
+  for (var i = 1; i <= 6; i++) {
+    c.push(<p className="item" id={i}> {data.text} </p>);
+  }
+  console.log(c)
+  return c
+}
+
 function GetContent() {
-  var l = ""
   fetch("./content")
     .then( response => {
       console.log(response)
@@ -21,19 +32,10 @@ function GetContent() {
         .then(data => 
           {
             console.log(data)
-            l = data.text
-            console.log(l)
+              return data
           }
         )
     })
-
-  var c = []
-  console.log(l)
-  for (var i = 1; i <= 6; i++) {
-    c.push(<p className="item" id={i}> {l} </p>);
-  }
-  console.log(c)
-  return c
 }
 
 // function displayContent(content) {
