98babdad145d0287f329f41e59f2b0f24d0f1f9e
cmelgreen <cmelgreen@gmail.com>
2020-11-02 17:47:33 -0600

    rewrite using hooks

--- frontend/src/components/content/content.js
+++ frontend/src/components/content/content.js
@@ -2,37 +2,30 @@ import React, { Component } from "react";
 import { useState, useEffect } from 'react';
 import "./content.css";
 
-const API = "./content"
+const API = "./content";
 
-class Content extends Component {
-  constructor(props) {
-    this.state = {content: ""};
-  }
+export default function Content() {
+  const [data, setData] = useState({content: []});
 
-  componentDidMount() {
-    fetch("./content")
-      .then(response => response.json)
-      .then(data => this.setState({
-        content: data.text
-      }))
-  }
-
-  render() {
-    const content = this.state;
-
-    let c = [];
-    for (var i = 1; i <= 6; i++) {
-      c.push(
-        <p className="item" key="{i}" id="{i}">
-          {" "}
-          {content.text}{" "}
-        </p>
-      );
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = fetch(API)
+        .then(response => response.json())
+        .then(data => {
+          setData({content: data});
+        });
     }
-    return c;
+    fetchData();
+  }, [])
 
+  let c = [];
+  for (var i = 1; i <= 6; i++) {
+    c.push(
+      <p className="item" key="{i}" id="{i}">
+        {" "}
+        {data.text}{" "}
+      </p>
+    );
   }
+  return c;
 }
-
-
-export default Content;
