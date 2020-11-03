56625eee29c114a5ad472db2b18ee84d48dd7562
cmelgreen <cmelgreen@gmail.com>
2020-11-02 18:10:43 -0600

    wrap content in div

--- frontend/src/components/content/content.js
+++ frontend/src/components/content/content.js
@@ -12,16 +12,11 @@ export default function Content() {
       const result = fetch(API)
         .then(response => response.json())
         .then(data => {
-          console.log(data)
           setData({content: data.text});
         });
     }
     fetchData();
-    console.log(data)
   }, [])
-
-  console.log(data)
-  console.log(data.content)
 
   let c = [];
   for (var i = 1; i <= 6; i++) {
@@ -32,6 +27,5 @@ export default function Content() {
       </p>
     );
   }
-  console.log("c: ", c)
-  return c;
+  return <div className="content">{c}</div>;
 }
