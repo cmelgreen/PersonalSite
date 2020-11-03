32eda7743d6978154f870e97cc125423e13d4cee
cmelgreen <cmelgreen@gmail.com>
2020-11-02 18:58:55 -0600

    use url-loader

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,24 +1,12 @@
-const path = require('path');
 module.exports = {
-    entry: './src/javascript/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
     module: {
-        rules: [
-            {
-                test: /\.(png|jpe?g)$/,
-                use: [{
-                    loader: "file-loader",
-                    options: {
-                    esModule: false,
-                    outputPath: "images",
-                    publicPath: "images",
-                    name: "[name].[ext]"
-                    }
-                }]
-            }
-        ]
-    }
+      rules: [
+        {
+          test: /\.(jpg|png)$/,
+          use: {
+            loader: 'url-loader',
+          },
+        },
+      ],
+    },
 };
