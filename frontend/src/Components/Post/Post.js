11ca8105bd4508c248e0e62280ed5bac55dc6dcd
Cooper Melgreen <cmelgreen@gmail.com>
2020-12-03 17:10:38 -0600

    add css to post

--- frontend/src/Components/Post/Post.js
+++ frontend/src/Components/Post/Post.js
@@ -1,10 +1,13 @@
+// modules
 import React  from 'react'
 import { useParams } from 'react-router-dom'
 import { Typography } from '@material-ui/core'
-
-import { HeaderContainer } from '../Header/Header'
-
+// utils
 import { getPostByID } from '../../Utils/ContentAPI'
+// components
+import { HeaderContainer } from '../Header/Header'
+// styles
+import './Post.css'
 
 export default function PostContainer() {
   const content = getPostByID(useParams().postID)
