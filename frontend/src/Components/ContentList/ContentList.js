a7d8926eaf95ea0302c72015d0644db16d531ad4
Cooper Melgreen <cmelgreen@gmail.com>
2020-12-03 15:59:18 -0600

    debug contentlist

--- frontend/src/Components/ContentList/ContentList.js
+++ frontend/src/Components/ContentList/ContentList.js
@@ -37,17 +37,18 @@ function ContentList(props) {
   return  (
     <div className='content-list'>
       <Grid container >
-        {props.posts.map((id, post) => (
-          <Grid item xs={12} sm={12} lg={6}>
+        {props.posts.map((id, post) => {
+          {console.log(post)}
+          return (<Grid item xs={12} sm={12} lg={6}>
             <ContentCardContainer 
               key={id} 
               id={id} 
               title={post.title} 
               summary={post.summary} 
               link={'post/' + post.title} 
-            />
+            />)
           </Grid>
-        ))}
+    })}
       </Grid>
     </div>
   )
