d04217d1e6c08880870b8092749c04b9035efb38
Cooper Melgreen <cmelgreen@gmail.com>
2020-12-03 13:40:47 -0600

    debug contentlist

--- frontend/src/Store/Store.js
+++ frontend/src/Store/Store.js
@@ -3,6 +3,7 @@ import { createStore } from 'redux'
 const initialValue = { summaries: [], validIds: ['AAA', 'BBB', 'CCC'] }
 
 const valueReducer = (state = initialValue, action) => {
+  console.log('type:', action.type, 'summaries:', action.summaries)
   switch (action.type) {
     case 'SET_CONTENT':
       return {...state,  content: action.content}
