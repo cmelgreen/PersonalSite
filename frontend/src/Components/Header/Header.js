2b39b3e29276a7dd1d47802897fa4f01662b703d
Cooper Melgreen <cmelgreen@gmail.com>
2020-12-03 18:15:29 -0600

    add header css

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -10,6 +10,7 @@ import {
   } from "@material-ui/core"
 
 // styles
+import './Header.css'
 import MenuIcon from '@material-ui/icons/Menu';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
@@ -18,30 +19,31 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 export function HeaderContainer(props) {
   const link = "/"
   const text = "Cooper Melgreen"
+
   return <Header {...props} text={text} link={link}/>
 }
 
 function Header(props) {
   return (
     <div className="header">
-    <AppBar position="static" >
-      <Toolbar>
-      <IconButton edge="start" aria-label="menu">
-        <Link to={props.link}>
-         <MenuIcon />
-        </Link>
-      </IconButton>
-        <Typography variant="h6">
-          {props.text}
-        </Typography>
-      <IconButton edge="end">
-        <LinkedInIcon />
-      </IconButton>
-      <IconButton edge="end">
-        <GitHubIcon />  
-      </IconButton>
-      </Toolbar>
-    </AppBar>
+      <AppBar position="static" >
+        <Toolbar>
+        <IconButton edge="start" aria-label="menu">
+          <Link to={props.link}>
+          <MenuIcon />
+          </Link>
+        </IconButton>
+          <Typography variant="h6">
+            {props.text}
+          </Typography>
+        <IconButton edge="end">
+          <LinkedInIcon />
+        </IconButton>
+        <IconButton edge="end">
+          <GitHubIcon />  
+        </IconButton>
+        </Toolbar>
+      </AppBar>
     </div>
   )
 }
