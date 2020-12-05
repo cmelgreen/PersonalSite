dfa44839f9affaa845e409794bde40dd1c33bd75
Cooper Melgreen <cmelgreen@gmail.com>
2020-12-04 22:53:40 -0600

    fix css

--- frontend/src/Components/CMS/CMSCardList/CMSCardList.js
+++ frontend/src/Components/CMS/CMSCardList/CMSCardList.js
@@ -11,7 +11,7 @@ export default function CMSPostList(props) {
   const posts = usePostSummaries()
   
   return (
-    <div className="cms-post-list">
+    <div className="cms-card-list">
       <Grid container direction="column">
           {posts.map((post, id) => (
             <Grid item xs={12} sm={12} lg={6}>
