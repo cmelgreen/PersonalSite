47e8b46828d6ae43ae7c6cb0dfbaff16c4d0e725
Cooper Melgreen <cmelgreen@gmail.com>
2020-12-05 18:45:15 -0600

    update cms summary card

--- frontend/src/Components/CMS/CMSSummaryCard/CMSSummaryCard.js
+++ frontend/src/Components/CMS/CMSSummaryCard/CMSSummaryCard.js
@@ -15,7 +15,12 @@ export default function CMSSummaryCard(props) {
   return (
     <div className="cms-summary-card">
       <Card>
-        {props.post.summary}
+        <CardHeader title={props.post.title} />
+          <CardContent>
+            <Typography color="textSecondary">
+              {props.post.summary}
+            </Typography>
+          </CardContent>
       </Card>
     </div>
   )
