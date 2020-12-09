14711d792e79c4c5e1d00372a8d301317cf21cf1
Cooper Melgreen <cmelgreen@gmail.com>
2020-12-09 15:25:16 -0600

    fix query

--- backend/database/dbConnector.go
+++ backend/database/dbConnector.go
@@ -139,7 +139,7 @@ func (db *Database) CreateTable(ctx context.Context) error {
 func (db *Database) QueryPost(ctx context.Context, title string) (models.Post, error) {
 	post := models.Post{}
 
-	query := "SELECT id, title, summary, raw_content FROM post WHERE title=$1;"
+	query := "SELECT id, title, summary, content FROM post WHERE title=$1;"
 	err := db.GetContext(ctx, &post, query, title)
 
 	return post, err
