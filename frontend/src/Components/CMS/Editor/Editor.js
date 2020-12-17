f84fa716d93136da741cf18cf2a235a03d552cba
Cooper Melgreen <cmelgreen@gmail.com>
2020-12-17 16:41:23 -0600

    fix editor

--- frontend/src/Components/CMS/Editor/Editor.js
+++ frontend/src/Components/CMS/Editor/Editor.js
@@ -1,22 +1,21 @@
-import React from "react";
+import React from 'react'
 
-import { useState } from "react";
-import { TextField } from "@material-ui/core";
-import MUIRichTextEditor from "mui-rte";
+import { useState } from 'react'
+import { TextField } from '@material-ui/core'
+import MUIRichTextEditor from 'mui-rte';
 import { useParams } from 'react-router-dom'
 
 import { createPost, usePostByID, useUpdatePostSummaries } from '../../../Utils/ContentAPI'
 
-import "./Editor.css"
+import './Editor.css'
 
 export default function Editor(props) {
   const id = useParams().postID
 
-  //id ? usePostByID(id, raw=true) :
-  const post = {title: 'Start typing...', summary: 'What are you writing about?', content: "start typing"}
+  const post = id ? usePostByID(id, raw=true) : {title: 'Title', summary: 'What are you writing about?', content: 'start typing'}
 
-  const [title, setTitle] = useState("Title")
-  const [summary, setSummary] = useState("Summary")
+  const [title, setTitle] = useState(post.title)
+  const [summary, setSummary] = useState(post.summary)
   const [tags, setTags] = useState('Tags')
 
   const onSave = (data) => {
@@ -25,39 +24,39 @@ export default function Editor(props) {
   }
 
   return (
-    <div className="editor">
-      <div className="title-editor">
+    <div className='editor'>
+      <div className='title-editor'>
         <TextField
-          id="outlined-basic"
-          label="Title"
+          id='outlined-basic'
+          label='Title'
           onChange={e => setTitle(e.target.value)}
-          variant="standard"
+          variant='standard'
           fullWidth={true}
         />
       </div>
-      <div className="summary-editor">
+      <div className='summary-editor'>
         <TextField
-          id="outlined-basic"
-          label="Summary"
+          id='outlined-basic'
+          label='Summary'
           onChange={e => setSummary(e.target.value)}
-          variant="standard"
+          variant='standard'
           fullWidth={true}
         />
       </div>
-      <div className="tags-editor">
+      <div className='tags-editor'>
         <TextField
-          id="outlined-basic"
-          label="Tags"
+          id='outlined-basic'
+          label='Tags'
           onChange={e => setTags(e.target.value.split(','))}
-          variant="standard"
+          variant='standard'
           fullWidth={true}
         />
       </div>
-      <div className="content-editor">
-        <MUIRichTextEditor onSave={onSave} />
+      <div className='content-editor'>
+        <MUIRichTextEditor defaultValue={} onSave={onSave} />
       </div>
     </div>
   );
 }
 
-//        <MUIRichTextEditor defaultValue={"Start typing"} onSave={onSave} />
+//        <MUIRichTextEditor defaultValue={'Start typing'} onSave={onSave} />
