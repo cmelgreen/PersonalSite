d19a1f4e4e331654aaf5faab6e7d8f442f50de4d
Cooper Melgreen <cmelgreen@gmail.com>
2020-12-17 20:38:32 -0600

    debug post

--- frontend/src/Store/Store.js
+++ frontend/src/Store/Store.js
@@ -5,6 +5,7 @@ import { NewPost } from "../Models/Posts"
 const initialValue = {summaries: [], post: NewPost()}
 
 const valueReducer = (state = initialValue, action) => {
+  console.log(action)
   switch (action.type) {
     case 'SET_POST':
       return {...state,  content: action.post}
