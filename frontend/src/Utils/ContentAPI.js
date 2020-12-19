f179376ee4ab0fb6a7dc8370bd340740088c8328
Cooper Melgreen <cmelgreen@gmail.com>
2020-12-18 19:53:19 -0600

    debug updatepost sql

--- frontend/src/Utils/ContentAPI.js
+++ frontend/src/Utils/ContentAPI.js
@@ -35,10 +35,10 @@ export const updatePost = (id, title, summary, data, tags) => {
   const post = {id: id, title: title, summary: summary, content: data}
 
   console.log(post)
-  
+
   axios.put(apiPost, post)
     .then(resp => console.log('Updated', resp))
-    .catch(resp => console.log('Error creating post', resp))
+    .catch(e => console.log('Error updating post', e))
 }
 
 export const usePostSummaries = (numPosts) => {
